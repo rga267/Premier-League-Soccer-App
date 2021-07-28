5a4dfe02381bfdef2523c3018565d72625f2de9d
Ricardo Gutierrez <rga267@nyu.edu>
2021-07-28 16:15:43 -0400

    added login authentication and restricted page views

--- users/urls.py
+++ users/urls.py
@@ -4,7 +4,8 @@ from . import views
 app_name ='users'
 
 urlpatterns = [
-    path('', views.login, name="login"),
+    path('', views.loginPage, name="login"),
+    path('logout/', views.logoutUser, name="logout"),
     path('signup/', views.signup, name="signup"),
     path('users/', views.home, name="home"),
 
--- users/views.py
+++ users/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
 def signup(request):
+    if request.user.is_authenticated:
+        return redirect('users:home')
+
     form = UserCreationForm()
 
     if request.method == 'POST':
@@ -13,15 +18,35 @@ def signup(request):
             form.save()
             user_name = form.cleaned_data.get('username')
             messages.success(request, 'Account was successfully created for ' + user_name+'.' + 'You can log in now!')
-            return redirect('login')
+            return redirect('users:login')
 
     context = {'form':form}
     return render(request, 'users/signup.html', context)
 
-def login(request):
+def loginPage(request):
+    if request.user.is_authenticated:
+        return redirect('users:home')
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('users:home')
+        else:
+            messages.info(request, 'Username OR password is incorrect!')
+
     context = {}
     return render(request, 'users/login.html', context)
 
+def logoutUser(request):
+    logout(request)
+    return redirect('users:login')
+
+@login_required(login_url='users:login')
 def home(request):
     context = {}
     return render(request, 'users/home.html', context)
